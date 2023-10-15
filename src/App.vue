c59028a83508eb19f0032b45c7c49ec6eba299a9
rensawamo <ren0112ngzk@gmail.com>
2023-10-15 19:47:56 +0900

    グラフ描写完了

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,92 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
-  <main></main>
+  <div id="app">
+    <div class="wrap">
+      <div>
+        <p class="check_title">都道府県</p>
+        <CheckPrefecture
+          @onAddSeries="addSeries"
+          @onRemoveSeries="removeSeries"
+        />
+      </div>
+      <div>
+        <Highcharts class="highchart" :options="chartOptions" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import { Chart } from "highcharts-vue";
+import CheckPrefecture from "./components/CheckPrefecture.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  components: {
+    Highcharts: Chart,
+    CheckPrefecture: CheckPrefecture,
+  },
+  data() {
+    return {
+      chartOptions: {
+        title: {
+          text: "都道府県別の総人口推移グラフ",
+        },
+        credits: {
+          enabled: false,
+        },
+        xAxis: {
+          lineWidth: 1,
+          tickWidth: 1,
+          title: {
+            align: "high",
+            x: 30,
+            offset: 15,
+            text: "年度",
+          },
+          categories: [
+            1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010,
+            2015, 2020, 2025, 2030, 2035, 2040, 2045,
+          ],
+        },
+        yAxis: {
+          lineWidth: 1,
+          tickWidth: 1,
+          title: {
+            align: "high",
+            offset: 10,
+            text: "人口数",
+            rotation: 0,
+            y: -20,
+          },
+        },
+        legend: {
+          align: "right",
+          verticalAlign: "top",
+          layout: "vertical",
+        },
+        series: [],
+      },
+    };
+  },
+  methods: {
+    addSeries: function (id, name, populations) {
+      this.chartOptions.series.push({
+        id: id,
+        name: name,
+        data: populations,
+      });
+    },
+    removeSeries: function (id) {
+      let index = this.chartOptions.series.findIndex(
+        (series) => series.id === id
+      );
+      if (index !== -1) {
+        this.chartOptions.series.splice(index, 1);
+      } else {
+        console.log("error delete");
+      }
+    },
+  },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/CheckPrefecture.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="wrap">
+    <div
+      v-for="prefecture in prefectures"
+      :key="prefecture.code"
+      class="prefecture"
+    >
+      <label :for="prefecture.code">
+        <input
+          type="checkbox"
+          :id="prefecture.code"
+          :checked="prefecture.isCreateGraph"
+          @click="
+            changeGraph(
+              prefecture.code,
+              prefecture.name,
+              prefecture.isCreateGraph
+            )
+          "
+        />
+        <span class="prefecture_name">{{ prefecture.name }}</span>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      prefectures: [],
+    };
+  },
+  mounted() {
+    this.onAppear();
+  },
+  methods: {
+    onAppear: function () {
+      this.getPrefecturesInfo("prefectures")
+        .then((response) => {
+          this.prefectures = response.data.result.map((res) => {
+            return {
+              code: res["prefCode"],
+              name: res["prefName"],
+              isCreateGraph: false,
+            };
+          });
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
+    getPrefecturesInfo: function (path) {
+      const response = axios.get(
+        `https://opendata.resas-portal.go.jp/api/v1/${path}`,
+        {
+          headers: { "X-API-KEY": import.meta.env.VITE_MY_API_KEY },
+        }
+      );
+      console.log("response", response);
+      return response;
+    },
+
+    createGraph: function (code, name) {
+      this.getPrefecturesInfo(
+        `population/composition/perYear?cityCode=-&prefCode=${code}`
+      )
+        .then((response) => {
+          var populations = [];
+          var rawPopulations = response.data.result.data[0].data;
+          for (let i = 0; i < rawPopulations.length; i++) {
+            populations.push(rawPopulations[i]["value"]);
+          }
+          this.$emit("onAddSeries", code, name, populations);
+          this.prefectures[code - 1].isCreateGraph = true;
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
+    deleteGraph: function (code) {
+      this.$emit("onRemoveSeries", code);
+      this.prefectures[code - 1].isCreateGraph = false;
+    },
+    changeGraph: function (code, name, isCreateGraph) {
+      if (isCreateGraph) {
+        this.deleteGraph(code);
+      } else {
+        this.createGraph(code, name);
+      }
+    },
+  },
+};
+</script>
+<style scoped></style>
